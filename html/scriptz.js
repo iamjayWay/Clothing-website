ef037ea344b71e5342b009ccf3abb55eeadaff1c
jay-Wayx <126787791+jay-Wayx@users.noreply.github.com>
2023-06-30 12:17:45 +0100

    first commit

---
+++ html/scriptz.js
@@ -0,0 +1,7 @@
+function showloginpage(){
+    var loginPage = document.getElementById("loginPage");
+
+    if(loginPage.style.display==="none"){
+        loginPage.style.display="block";
+    } else{loginPage.style.display="none"}
+}
